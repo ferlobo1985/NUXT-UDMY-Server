44301f62ea5c302a1a40788373e4b85069464e5a
Coding rev <fernando.lobo.prez@gmail.com>
2025-09-24 03:25:34 -0300

    04-Fetching data
    04-Fetching data

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1>Home</h1>
+    <h1>Employees:</h1>
+    <div v-if="pending">Loading...</div>
+    <div v-else-if="error">Error:{{ error.message }}</div>
+    <div v-else class="row row-cols-1 row-cols-sm-2 rows-cols-md-3 g-3">
+        <div v-for="employee in data" :key="employee.id" class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">
+                        {{  employee.fullname }}
+                    </h3>
+                    <hr/>
+                    <p class="card-text">{{ employee.position }}</p>
+                    <p class="card-text">{{ employee.age }}</p>
+                    <nuxtLink :to="`/employees/${employee.id}`" class="btn btn-primary">
+                        Details
+                    </nuxtLink>
+                </div>
+               
+            </div>
+        </div>
+
+    </div>
 </template>
+
+<script setup>
+// $fetch, useFetch, useAsyncFetch
+    // const data = await $fetch('http://localhost:3004/employees')
+    // console.log(data)
+
+    // const {error, pending, data} = await useFetch('http://localhost:3004/employees')
+
+     const {error, pending, data} = await useAsyncData('someData',()=>{
+        return $fetch('http://localhost:3004/employees')
+     })
+</script>
