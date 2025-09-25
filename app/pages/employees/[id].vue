8c6748ba18cc3ea8a3087b4e24ae8862ef16f4ba
Coding rev <fernando.lobo.prez@gmail.com>
2025-09-25 03:50:27 -0300

    18-Employee by id page
    18-Employee by id page

--- app/pages/employees/[id].vue
+++ app/pages/employees/[id].vue
@@ -1,3 +1,27 @@
 <template>
     <h1>Employee by ID</h1>
+    <div v-if="pending">Loading...</div>
+    <div v-else-if="error">Sorry try again later</div>
+    <div v-else>
+        <p><strong>Full name</strong>:{{  data.fullname }}</p>
+        <p><strong>Position</strong>:{{  data.postion }}</p>
+        <p><strong>Age</strong>:{{  data.age }}</p>
+    </div>
+    <hr/>
+    <button class="btn btn-primary" @click="editEmployee">
+        Edit employee
+    </button>
 </template>
+
+<script setup>
+    const route = useRoute()
+    const router = useRouter();
+
+    /// REDIRECT TO EDIT
+    function editEmployee(){
+        router.push(`/form/edit/${route.params.id}`)
+    }
+
+    /// GET EMPLOYEE BY ID
+    const {error,pending,data} = await useFetch(`/api/employees/byid?id=${route.params.id}`);
+</script>
