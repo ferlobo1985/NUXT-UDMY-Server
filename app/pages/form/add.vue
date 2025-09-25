2ba7a02016a97a64d254fea3332eec7a51ae8a07
Coding rev <fernando.lobo.prez@gmail.com>
2025-09-25 01:26:53 -0300

    10-Posting Employees
    10-Posting Employees

--- app/pages/form/add.vue
+++ app/pages/form/add.vue
@@ -1,17 +1,17 @@
 <template>
     <h1>Add Employee</h1>
-    <form class="mt-4" @submit.prevent="submitForm"">
+    <form class="mt-4" @submit.prevent="submitForm">
         <div class="mb-3">
             <label for="fullname" class="form-label">Full Name</label>
             <input v-model="formData.fullname" type="text" class="form-control" id="fullname" required/>
         </div>
         <div class="mb-3">
-            <label for="fullname" class="form-label">Position</label>
-            <input v-model="formData.position" type="text" class="form-control" id="fullname" required/>
+            <label for="position" class="form-label">Position</label>
+            <input v-model="formData.position" type="text" class="form-control" id="position" required/>
         </div>
         <div class="mb-3">
-            <label for="fullname" class="form-label">Age</label>
-            <input v-model="formData.age" type="number" class="form-control" id="fullname" required/>
+            <label for="age" class="form-label">Age</label>
+            <input v-model="formData.age" type="number" class="form-control" id="age" required/>
         </div>
         <button type="submit" class="btn btn-primary">Add Employee</button>
     </form>
@@ -27,11 +27,17 @@
     });
 
     async function submitForm(){
-        const request = await $fetch('/api/employees?example1=yes&example2=no',{
-            method:'post',
-            body:formData.value
-        })
-        console.log(request)
+        try {
+            await $fetch('/api/employees',{
+                method:'post',
+                body:formData.value
+            })
+        } catch (error) {
+            console.log(error)
+        }
+
+        
+        // console.log(request)
     }
 
 </script>
--- server/api/employees/index.post.js
+++ server/api/employees/index.post.js
@@ -1,9 +1,21 @@
 export default defineEventHandler(async(event)=>{
-    const body = await readBody(event);
-    const query = getQuery(event)
+    try{
+        const body = await readBody(event);
 
-    return {
-       body,
-       query
+        if(body.fullname.length <= 3){
+            throw createError({
+                statusCode:404,
+                statusMessage:'Name is too short'
+            })
+        }
+
+        const data = await $fetch('http://localhost:3004/employees',{
+            method:'POST',
+            body
+        })
+
+        return data
+    } catch(error){
+        throw createError(error)
     }
 })
