1787a01413eeeeaa4600c65a741c822f60859629
Coding rev <fernando.lobo.prez@gmail.com>
2025-09-25 00:59:49 -0300

    08-Add employee form
    08-Add employee form

--- app/pages/form/add.vue
+++ app/pages/form/add.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>ADD EMPLOYEE</h1>
+    <h1>Add Employee</h1>
+    <form class="mt-4" @submit.prevent="submitForm"">
+        <div class="mb-3">
+            <label for="fullname" class="form-label">Full Name</label>
+            <input v-model="formData.fullname" type="text" class="form-control" id="fullname" required/>
+        </div>
+        <div class="mb-3">
+            <label for="fullname" class="form-label">Position</label>
+            <input v-model="formData.position" type="text" class="form-control" id="fullname" required/>
+        </div>
+        <div class="mb-3">
+            <label for="fullname" class="form-label">Age</label>
+            <input v-model="formData.age" type="number" class="form-control" id="fullname" required/>
+        </div>
+        <button type="submit" class="btn btn-primary">Add Employee</button>
+    </form>
 </template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const formData =  ref({
+        fullname:'',
+        position: '',
+        age: null
+    });
+
+    function submitForm(){
+        console.log(formData.value)
+    }
+
+</script>
